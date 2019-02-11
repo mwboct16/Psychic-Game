8cad5a33dc1e08149c6d243b78fb93f4082afa7a
Michael Black <mwboct16@gmail.com>
2019-02-10 19:13:26 -0500

    Attempted HTML in js, would not generate!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,21 +13,55 @@ var currentGuesses = [];
 var winsText = document.getElementById("wins");
 var lossText = document.getElementById("loss");
 var remainingGuessesText = document.getElementById("remaining-guesses");
-var guessesText = document.getElementById("guesses");
+var guessesText = document.getElementById("guesses"); 
 
 // Computer decision
-var computerChoice = function(){
+var computerChoice = function () {
     computerDecision = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
+
 computerChoice();
-// console.log(computerDecision);
+console.log(computerDecision);
 
- document.onkeyup = function(){
+document.onkeyup = function (event) {
     
+    var userGuess = event.key;
 
 
+    if (userGuess === computerDecision) {
+        totalWins++;
+        guessesLeft = 9;
+        currentGuesses.length = 0;
+        console.log(totalWins);
+    } else if (userGuess != computerDecision) {
+        guessesLeft--;
+        currentGuesses.push(userGuess);
+        console.log(currentGuesses);
+        console.log(guessesLeft);
+    }
+    if (guessesLeft === 0) {
+        totalLost++;
+        guessesLeft = 9;
+        currentGuesses.length = 0;
+        console.log(totalLost);
+    }
+
+/* Every time I've put this code in, an Error popped up in the conosole and won't allow my results 
+to pop up in my HTML 
+
+winsText.textContent = "Wins: " + totalWins;
+lossText.textContent = "Losses: " + totalLost;
+guessesText.textContent = "Your guesses so far: " + currentGuesses.join("");
+remainingGuessesText.textContent = "Guesses Left: " + guessesLeft;
+console.log(winsText);
+console.log(lossText);
+console.log(guessesText); 
+
+*/
 
 };
+  
+
 
 
 
